337fc4785098fac5e18d5bb42195049226913d6b
tthau02 <tranhau201202@gmail.com>
2025-05-15 23:55:50 +0700

    upload date fix error

--- src/components/layouts/client/ClientSearch.tsx
+++ src/components/layouts/client/ClientSearch.tsx
@@ -300,7 +300,7 @@ export default function ClientSearch() {
                   month={month}
                   onMonthChange={setMonth}
                   weekStartsOn={1}
-                  disabled={(date) => date < new Date()}
+                  disabled={(date: any) => date < new Date()}
                   locale={vi}
                   className="p-0"
                   showOutsideDays={false}
