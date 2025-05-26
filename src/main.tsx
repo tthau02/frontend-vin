0b7c845212d8d34b8c59a25c3d9750c6bdad96e1
ledoan05 <lecongdoan05@gmail.com>
2025-05-26 13:53:00 +0700

    set up redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
 import './index.css';
-import { router } from '@/routes';
-import { QueryProvider } from '@/context/QueryProvider';
+import { AppProviders } from '@/context/AppProviders';
+import App from './App';
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryProvider>
-      <RouterProvider router={router} />
-    </QueryProvider>
-  </StrictMode>,
+    <AppProviders>
+      <App />
+    </AppProviders>
+  </StrictMode>
 );
